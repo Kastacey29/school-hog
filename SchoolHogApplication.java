527893d988535bb99bc8e299afa726ff7b1d12d7
Stacey29 <Kastacey@yandex.ru>
2023-01-14 20:37:18 +0300

    first commit

---
+++ SchoolHogApplication.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.schoolhog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolHogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolHogApplication.class, args);
+    }
+
+}
